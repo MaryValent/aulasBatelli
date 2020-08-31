9ec195508e3ec18d12c14ac45611f997f97eddde
Valentina <midnanakamura@gmail.com>
2020-08-31 09:13:29 -0300

    inicio
    inicio do projeto, main menu já está funcional e o menu de pause in-game também

---
+++ Trabalho do batelli/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+
+    public AudioMixer audioMixer;
+    //array com as resolucoes
+    Resolution[] resolutions;
+    //barra da resolucao
+    public Dropdown resolutionDropdown;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        //lista porque string n pode em array
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        //ligacao para poder pegar todas as resolucoes que o computador permite
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        //pegando a resolucao atual do computador
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+
+    //unity percebendo as resolucoes permitidas pelo computador
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("MainVolume", volume);
+
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+
+}
---
+++ Trabalho do batelli/Assets/Scripts/mainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    //arrumando a animacao da transicao entre cenas
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    public void PlayGame()
+    {
+        //avisando que o botao da load na outra cena pelo coroutine da transicao
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    //arrumando a transicao em ordem com tempo de espera e carregamento da cena
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        //animacao
+        transition.SetTrigger("Start");
+        //esperar
+        yield return new WaitForSeconds(transitionTime);
+        //carregar cena
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    //botao de sair do jogo
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
